59cc0e038aeae200a1ab1cce687d86f719842f7d
Tedec1 <jaspermesenbrink@gmail.com>
2022-07-21 14:30:54 -0600

    added ip address text input

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [ip, setIp] = useState("")
+  const [loading, setLoading] = useState(false)
+  const handleOnSubmit = (e) => {
+    try {
+      setLoading(true)
+
+
+
+    
+    } catch (error) {
+      console.error(error);
+    } finally{
+      setLoading(false)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter the address of the edge server you want to connect to</Text>
+      <TextInput 
+      style={{outline:'1px solid', margin:'10px'}} 
+      onChange={(e)=>setIp(e.nativeEvent.text)}
+      value={ip}
+      placeholder="225.225.225.1:3000"
+      keyboardType="numeric"
+      ></TextInput>
+      <Button 
+      disabled={loading}
+        onPress={handleOnSubmit}
+        title="Submit"
+        // color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +44,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
