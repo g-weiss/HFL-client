6b92c81d8ee33b654a366041a2791d66919935c0
Garrett Weissert <garratt106@gmail.com>
2023-03-08 15:58:27 -0700

    fixed util to match others

--- util.js
+++ util.js
@@ -1,34 +1,25 @@
-
 const callApi = async ({ url, method, token, body }) => {
-    // console.log("callApi: ", { url, method, token, body });
-    try {
-      const options = {
-        method: method ? method.toUpperCase() : "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      }
-      if (token) {
-        options.headers["Authorization"] = `Bearer ${token}`
-      }
-      // console.log("request url: ", BASE_URL + url)
-      // console.log("options: ", options)
-      const response = await fetch(`${url}`, options)
-    //   const data = await response.json()
-      // console.log("data: ", data)
-
-    //   if (data.error) throw data.error
-    //   return data
-      return response
-
-    } catch (error) {
-      console.error("ERROR: ", error)
-      return error
+  try {
+    const options = {
+      method: method ? method.toUpperCase() : "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
     }
-
+    if (token) {
+      options.headers["Authorization"] = `Bearer ${token}`
+    }
+    const response = await fetch(`http://${url}`, options)
+    const data = await response.json()
+    if (data.error) throw data.error
+    return data
+  } catch (error) {
+    console.error("ERROR: ", error.error, error.message)
+    return error
   }
+}
 
-export {
-    callApi
+module.exports = {
+  callApi
 }
